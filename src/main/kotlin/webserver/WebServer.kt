fa0f5c743d92acf3782db565570a33b1258827e0
Bill Yang <billthechurch@dyn3210-36.wlan.ic.ac.uk>
2022-12-06 14:17:32 +0000

    first commit

--- src/main/kotlin/webserver/WebServer.kt
+++ src/main/kotlin/webserver/WebServer.kt
@@ -4,70 +4,73 @@ package webserver
 
 // write your web framework code here:
 
-fun scheme(url: String): String =
-  url.substringBefore("://")
+fun scheme(url: String): String = url.substringBefore("://")
 
 fun host(url: String): String {
-  return url.substringAfter("://").substringBefore("/")
+    return url.substringAfter("://").substringBefore("/")
 }
 
 fun path(url: String): String {
-  return url.substringAfter(host(url)).substringBefore("?")
+    return url.substringAfter(host(url)).substringBefore("?")
 }
 
 fun queryParams(url: String): List<Pair<String, String>> {
-  return if (url.contains("?")) {
-    val query = url.substringAfter("?").substringBefore(".")
-    val listOfQuery = query.split("&", "=").chunked(2)
-    listOfQuery.map { x -> Pair(x[0], x[1]) }
-  } else {
-    listOf()
-  }
+    return if (url.contains("?")) {
+        val query = url.substringAfter("?").substringBefore(".")
+        val listOfQuery = query.split("&", "=").chunked(2)
+        listOfQuery.map { x -> Pair(x[0], x[1]) }
+    } else {
+        listOf()
+    }
 }
+/* ALT for fun queryParams
+if (! url.contains("?")) {
+return emptylist()
+}
+val params = url.substringAfter("?").split("&")
+val kvs    = params.map { p -> p.split("=") }
+return kvs.map { token -> Pair(token[0], token[1]) }
+*/
 
 fun route(request: Request): Response {
-  val url = request.url
-  return when (path(url)) {
-    "/" -> homePageHandler()
-    "/say-hello" -> helloHandler(request)
-    "/computing" -> computingHandler()
-    else -> errorHandler()
-  }
+    val url = request.url
+    return when (path(url)) {
+        "/" -> homePageHandler()
+        "/say-hello" -> helloHandler(request)
+        "/computing" -> computingHandler()
+        else -> errorHandler()
+    }
 }
 
 // http handlers for a particular website...
-fun homePageHandler(): Response =
-  Response(Status.OK, "This is Imperial.")
+fun homePageHandler(): Response = Response(Status.OK, "This is Imperial.")
 
-fun computingHandler(): Response =
-  Response(Status.OK, "This is DoC.")
+fun computingHandler(): Response = Response(Status.OK, "This is DoC.")
 
-fun errorHandler(): Response =
-  Response(Status.NOT_FOUND)
+fun errorHandler(): Response = Response(Status.NOT_FOUND)
 
-fun nameHandler(name: String, param: String): String =
-  "Hello, $param!"
+fun nameHandler(name: String, param: String): String = "Hello, $param!"
 
 fun styleHandler(sentence: String, param: String): String {
-  return when (param) {
-    "shouting" -> sentence.uppercase()
-    "whispering" -> sentence.lowercase()
-    else -> sentence
-  }
+    return when (param) {
+        "shouting" -> sentence.uppercase()
+        "whispering" -> sentence.lowercase()
+        else -> sentence
+    }
 }
 
 fun helloHandler(request: Request): Response {
-  val queryParam = queryParams(request.url)
-  val paramHandler = mapOf(
-    Pair("name", ::nameHandler),
-    Pair("style", ::styleHandler)
-  )
-  var hello = "Hello, World!"
-  for (param in queryParam) {
-    paramHandler[param.first]!!.invoke(hello, param.second).also { hello = it }
-  }
-  return Response(
-    Status.OK,
-    hello
-  )
+    val queryParam = queryParams(request.url)
+    val paramHandler = mapOf(
+        Pair("name", ::nameHandler),
+        Pair("style", ::styleHandler)
+    )
+    var hello = "Hello, World!"
+    for (param in queryParam) {
+        paramHandler[param.first]!!.invoke(hello, param.second).also { hello = it }
+    }
+    return Response(
+        Status.OK,
+        hello
+    )
 }
